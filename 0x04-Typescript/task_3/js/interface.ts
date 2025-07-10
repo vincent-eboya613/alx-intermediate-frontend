8f499d369112761d0a650b5b0e4aed4de2094b84
kimoban <67864381+kimoban@users.noreply.github.com>
2025-07-10 19:40:50 +0000

    Delete 0x04-Typescript/task_3 directory

--- 0x04-Typescript/task_3/js/interface.ts
+++
@@ -1,9 +0,0 @@
-// Define RowID as a number
-export type RowID = number;
-
-// Define RowElement interface with optional age property
-export interface RowElement {
-    firstName: string;
-    lastName: string;
-    age?: number;
-}
