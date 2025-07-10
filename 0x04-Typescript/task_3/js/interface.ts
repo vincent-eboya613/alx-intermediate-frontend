6592279352bc058085fe0bd07c1079a8d8ae37d6
vincent-eboya613 <vincentkidavi@gmail.com>
2025-07-10 17:52:02 +0300

    Create interface.ts

---
+++ 0x04-Typescript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+// Define RowID as a number
+export type RowID = number;
+
+// Define RowElement interface with optional age property
+export interface RowElement {
+    firstName: string;
+    lastName: string;
+    age?: number;
+}
